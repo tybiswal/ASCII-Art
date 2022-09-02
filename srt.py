3a63f9f9474af6ed141c9b30477c8cdc156186d1
tybiswal <kanhatheman@gmail.com>
2022-09-02 23:27:17 +0630

    Minor changes and commenting out the color inquiry input

--- srt.py
+++ srt.py
@@ -1,9 +1,8 @@
 # This code helps to print ascii art in terminal
 import pyfiglet
-from termcolor import colored
-
+# from termcolor import colored
 msg = input("What word would you like to print in ASCII?: ")
-clr = input("What color should it be in?: ")
-ascii_art = pyfiglet.figlet_format(msg)
-colored_ascii = colored(ascii_art, color=clr)
-print(colored_ascii)
+# clr = input("What color should it be in?: ")
+ascii_art = pyfiglet.figlet_format(msg, font='bulbhead')
+# colored_ascii = colored(ascii_art, clr)
+print(ascii_art)
