4f2e3606aae15492ec72dbd1ee047c814e4cd7df
tybiswal <kanhatheman@gmail.com>
2022-09-02 17:09:37 +0630

    Added input to change color of ascii output

--- srt.py
+++ srt.py
@@ -1,10 +1,9 @@
 # This code helps to print ascii art in terminal
-
 import pyfiglet
 from termcolor import colored
 
 msg = input("What word would you like to print in ASCII?: ")
+clr = input("What color should it be in?: ")
 ascii_art = pyfiglet.figlet_format(msg)
-# colored_ascii = colored(ascii_art, color ="red")
-print(ascii_art)
-
+colored_ascii = colored(ascii_art, color=clr)
+print(colored_ascii)
